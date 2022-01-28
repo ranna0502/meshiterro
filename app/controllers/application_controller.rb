0e9b24a5f66f0ff1db9768780a01d37576b9b0b9
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-01-28 03:11:54 +0000

    [finish]4-13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
+  # deviseのデフォルトメソッドを処理する前に定義したメソッドを処理する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    # サインイン後にどこに遷移するかを設定する(一覧画面へ)
+    def after_sign_in_path_for(resource)
+      post_images_path
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def after_sign_out_path_for(resource)
+      about_path
+    end
+
+
+
+    protected
+
+    #nameカラムの受け取りをきょかする
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
